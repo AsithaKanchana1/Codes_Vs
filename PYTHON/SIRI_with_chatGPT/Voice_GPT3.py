f2699419f62bbe239a17f5174020617ad45490ba
asitha <asitha.educational@gmail.com>
2023-04-10 19:12:15 +0530

    Siri_Powerd_by_chatGPT3_v1_20200410

---
+++ PYTHON/SIRI_with_chatGPT/Voice_GPT3.py
@@ -0,0 +1,68 @@
+import openai
+import pyttsx3
+import speech_recognition as sr
+import time
+
+#set your openAi API key
+openai.api_key ="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"
+
+#Initialize the text-to-speech engin
+engine =pyttsx3.init()
+
+def transcribe_audio_to_text(filename):
+    recognizer = sr.Recognizer()
+    with sr.AudioFile(filename) as source:
+        audio = recognizer.record(source)
+    try:
+        return recognizer.recognize_google(audio)
+    except:
+        print('Skipping Unknown error...')
+def generate_response(prompt):
+    response = openai.Completion.create(
+        engine="text-davinci-003",
+        prompt=prompt,
+        max_tokens=4000,
+        stop=None,
+        temperature=0.5,
+    )
+    return response["choices"][0]["text"]
+
+def speak_text(text):
+    engine.say(text)
+    engine.runAndWait()
+
+def main():
+    while True:
+        #waite for user to say "genius"
+        print("say 'Genius' to start recoding your question...")
+        with sr.Microphone() as source:
+            recognizer = sr.Recognizer()
+            audio = recognizer.listen(source)
+            try:
+                transcription = recognizer.recognize_google(audio)
+                if transcription.lower() == "genius":
+                    #recode odio
+                    filename = "input.wav"
+                    print("Say your question...")
+                    with sr.Microphone() as source:
+                        recognizer = sr.Recognizer()
+                        source.pause_threshold = 1
+                        audio = recognizer.listen(source, phrase_time_limit=None, timeout=None)
+                        with open(filename, "wd") as f:
+                            f.write(audio.get_wav_data())
+
+                    #transcribe audio to text
+                    text = transcribe_audio_to_text(filename)
+                    if text:
+                        print(f"You said:" (text))
+
+                        #genarate response using GPT-3
+                        response = generate_response(text)
+                        print("GPT-3 says:"(response))
+
+                        #read response using text-to-speech
+                        speak_text(response)
+            except Exception as e:
+                print("An error occurrd:{}".format(e))
+if __name__ == "__main__":
+    main()
